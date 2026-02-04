3b30c63a37af36f12c1981e16f58468608169d36
Duarte Escairo <a106936@alunos.uminho.pt>
2026-02-04 22:21:14 +0000

    Feito ?

--- TP1/json2html.py
+++ TP1/json2html.py
@@ -19,11 +19,15 @@ def new_file(filename, content):
 
 
 dataset = open_json('TP1/dataset_reparacoes.json')
+mk_dir("TP1/output")
 # -------------------------------------------------------------------------------------------
 
-n = 0
-links_reparacoes = ""
+n = 1
 reparacoes = ""
+links_reparacoes = ""
+links_intervencoes = ""
+infos_intervencoes = {}
+infos_marcas = {}
 for reparacao in dataset["reparacoes"]:
      
      links_reparacoes += f'''
@@ -31,10 +35,10 @@ for reparacao in dataset["reparacoes"]:
           <a href="r{n}.html"> Reparação {n} </a>
      </li>
      '''
-     n += 1
 
      marca = reparacao["viatura"]["marca"]
      modelo = reparacao["viatura"]["modelo"]
+     viatura = reparacao["viatura"]["matricula"]
 
      reparacao2 = f'''
      <html>
@@ -57,15 +61,138 @@ for reparacao in dataset["reparacoes"]:
           </body>
      </html>
      '''
-     new_file("./TP1/output/reparacao.html", reparacao2)
+     new_file(f"./TP1/output/r{n}.html", reparacao2)
 
-# -------------------------------------------------------------------------------------------
+     for intervencao in reparacao["intervencoes"]:
+          codigo = intervencao["codigo"]
 
+          if codigo not in infos_intervencoes:
+               infos_intervencoes[codigo] = {
+                    "codigo": intervencao["codigo"],
+                    "nome": intervencao["nome"],
+                    "descricao": intervencao["descricao"],
+                    "reparacoes": []
+               }
 
+          infos_intervencoes[codigo]["reparacoes"].append(n)
 
-# -------------------------------------------------------------------------------------------
+     n += 1
 
+     if marca not in infos_marcas:
+          infos_marcas[marca] = {
+               "modelos": {}
+     }
+     if modelo not in infos_marcas[marca]["modelos"]:
+          infos_marcas[marca]["modelos"][modelo] = set()
 
+     infos_marcas[marca]["modelos"][modelo].add(viatura)
+
+for codigo in sorted(infos_intervencoes.keys()):
+     info = infos_intervencoes[codigo]
+
+     links_intervencoes += f"""
+     <li>
+          <a href="{info["codigo"]}.html"> {info["codigo"]} - {info["nome"]} - {info["descricao"]} </a>
+     </li>
+     """
+
+     lista_reparacoes = ""
+     for r in info["reparacoes"]:
+          lista_reparacoes += f'<li><a href="r{r}.html">Reparação {r}</a></li>'
+
+     intervencao = f'''
+     <html>
+          <head>
+               <title> Intervenção {info["codigo"]} </title>
+               <meta charset="utf-8"/>
+          </head>
+          <body>
+               <h3>  Intervenção {info["codigo"]} </h3>
+               <table border="1">
+                    <tr> <td> Código </td> <td> {info["codigo"]} </td> </tr>
+                    <tr> <td> Nome </td> <td> {info["nome"]} </td> </tr>
+                    <tr> <td> Descrição </td> <td> {info["descricao"]} </td> </tr>
+               </table>
+               <h4>Reparações</h4>
+               <ul>
+               {lista_reparacoes}
+               </ul>
+
+               <hr/>
+
+          </body>
+     </html>
+     '''
+     new_file(f'./TP1/output/{info["codigo"]}.html', intervencao)
+
+# ------------------- PAGINAS MARCAS E MODELOS -------------------
+
+links_marcas = ""
+
+for marca in sorted(infos_marcas.keys()):
+    info = infos_marcas[marca]
+
+    links_marcas += f'''
+    <li>
+        <a href="{marca}.html"> Marca: {marca} </a>
+    </li>
+    '''
+
+    modelos_links = ""
+
+    for modelo in sorted(info["modelos"].keys()):
+
+        # criar página do modelo
+        matriculas = ""
+        for m in sorted(info["modelos"][modelo]):
+            matriculas += f"<li>{m}</li>"
+
+        pag_modelo = f'''
+        <html>
+            <head>
+                <title>{marca} - {modelo}</title>
+                <meta charset="utf-8"/>
+            </head>
+            <body>
+                <h3>{marca} - {modelo}</h3>
+                <h4>Matrículas</h4>
+                <ul>
+                    {matriculas}
+                </ul>
+                <hr/>
+                <a href="{marca}.html">Voltar à marca</a>
+            </body>
+        </html>
+        '''
+
+        new_file(f"./TP1/output/{marca}_{modelo}.html", pag_modelo)
+
+        modelos_links += f'''
+        <li>
+            <a href="{marca}_{modelo}.html">{modelo}</a>
+        </li>
+        '''
+
+    # criar página da marca
+    pag_marca = f'''
+    <html>
+        <head>
+            <title>{marca}</title>
+            <meta charset="utf-8"/>
+        </head>
+        <body>
+            <h3>{marca}</h3>
+            <p>Número de modelos: {len(info["modelos"])}</p>
+            <ul>
+                {modelos_links}
+            </ul>
+            <hr/>
+            <a href="marcas_modelos.html">Voltar às marcas</a>
+        </body>
+    </html>
+    '''
+
+    new_file(f"./TP1/output/{marca}.html", pag_marca)     
 
 # -------------------------------------------------------------------------------------------
 
@@ -83,7 +210,7 @@ html = f'''
                     </li>
 
                     <li>
-                         <a href="links_reparacoes.html"> Listagem dos tipos de intervenção </a>
+                         <a href="intervencoes.html"> Listagem dos tipos de intervenção </a>
                     </li>
 
                     <li>
@@ -95,8 +222,7 @@ html = f'''
 
 '''
 
-mk_dir("TP1/output")
 new_file("./TP1/output/index.html", html)
-new_file("./TP1/output/links_reparacoes.html", links_reparacoes)
-new_file("./TP1/output/intervencoes.html", html)
-new_file("./TP1/output/marcas_modelos.html", html)
+new_file("./TP1/output/reparacoes.html", links_reparacoes)
+new_file("./TP1/output/intervencoes.html", links_intervencoes)
+new_file("./TP1/output/marcas_modelos.html", links_marcas)
