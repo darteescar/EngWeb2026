fc635269b9003a9b011735785402d3353a02813d
Duarte Escairo <a106936@alunos.uminho.pt>
2026-02-05 22:22:24 +0000

    Intervenções feitas

--- TP1/script.py
+++ TP1/script.py
@@ -20,19 +20,22 @@ def new_file(filename, content):
 dataset = open_json('TP1/dataset_reparacoes.json')
 mk_dir("TP1/output")
 
+## Pagina Reparacões
 links_reparacoes = ""
-
 n = 1
+
+## Pagina Tipos de Intervenção
+tipos_intervencao = {}
+links_intervencoes = ""
+
 for rep in dataset["reparacoes"]:
 
      ## Pagina Reparacões
-
      links_reparacoes += f'''
      <li>
           <a href="reparacao{n}.html"> Reparação {n} </a>
      </li>
      '''
-
      pagina_reparacao = f'''
      <html>
           <head>
@@ -54,8 +57,63 @@ for rep in dataset["reparacoes"]:
           </body>
      </html>
      '''
-
      new_file(f"./TP1/output/reparacao{n}.html", pagina_reparacao)
+
+     ## Pagina Tipos de Intervenção
+     for intervencao in rep["intervencoes"]:
+          codigo = intervencao["codigo"]
+          if codigo not in tipos_intervencao:
+               tipos_intervencao[codigo] = {
+                    "nome": intervencao["nome"],
+                    "descricao": intervencao["descricao"],
+                    "reparacoes": []
+          }
+     tipos_intervencao[codigo]["reparacoes"].append(n)
+
+     n += 1
+
+n = 1
+
+for cod_de_intervencao in sorted(tipos_intervencao.keys()):
+     intervencao = tipos_intervencao[cod_de_intervencao]
+     
+     ## Links para a página principal das intervenções
+     links_intervencoes += f'''
+     <li>
+          <a href="intervencao{n}.html"> Intervenção {n} </a>
+     </li>
+     '''
+
+     lista_reparacoes_desta_intervencao = ""
+     for reparacao in intervencao["reparacoes"]:
+          lista_reparacoes_desta_intervencao += f'''
+          <li>
+               <a href="reparacao{reparacao}.html"> Reparação {reparacao} </a>
+          </li>
+          '''
+
+     pagina_intervencao = f'''
+     <html>
+          <head>
+               <title> Intervenção nº {n} </title>
+               <meta charset="utf-8"/>
+          </head>
+          <body>
+               <h3>  Intervenção nº {n} </h3>
+               <table border="1">
+                    <tr> <td> Código </td> <td> {cod_de_intervencao} </td> </tr>
+                    <tr> <td> Nome </td> <td> {intervencao["nome"]} </td> </tr>
+                    <tr> <td> Descrição </td> <td> {intervencao["descricao"]} </td> </tr>
+               </table>
+               <h3> Reparações que precisaram desta intervenção: </h3>
+               <ul>
+                    {lista_reparacoes_desta_intervencao}
+               </ul>
+               <hr/>
+          </body>
+          </html>
+     '''
+     new_file(f"./TP1/output/intervencao{n}.html", pagina_intervencao)
 
      n += 1
 
@@ -96,10 +154,23 @@ pagina_reparacoes = f'''
           </ul>
      </body>
 '''
-pagina_intervencoes = ""
+pagina_intervencoes = f'''
+<html>
+     <head>
+          <title> Lista de intervenções </title>
+          <meta charset="utf-8"/>
+     </head>
+     <body>
+          <h3> Lista de intervenções </h3>
+          <ul>
+               {links_intervencoes}
+          </ul>
+     </body>
+'''
 pagina_marcas_e_modelos = ""
 
 
 
 new_file("./TP1/output/index.html", pagina_inicial)
 new_file("./TP1/output/pagina_reparacoes.html", pagina_reparacoes)
+new_file("./TP1/output/pagina_intervencoes.html", pagina_intervencoes)
